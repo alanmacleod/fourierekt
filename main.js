66d9de71916fa949e7b189423542f7eece815bb6
Alan MacLeod <amacleod@rskorbital.com>
2018-01-20 23:49:43 +0700

    lol it works

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 const BITTREX_PROXY = "http://localhost:3000/";
-const CHART_WIDTH = 640, CHART_HEIGHT= 480;
+const CHART_WIDTH = 1200, CHART_HEIGHT= 600;
 const BAR_WIDTH = 3, BAR_PADDING = 4;
 const BAR_FOOTPRINT = BAR_WIDTH + BAR_PADDING;
 
@@ -16,14 +16,23 @@ c.style.height = `${CHART_HEIGHT}px`;
 
 var ctx = c.getContext("2d");
 
-console.log(MAX_DISPLAY_BARS);
+var pair = "BTC-SC";
+var exchange = "BITTREX"
+const INTERVALS = [
+  ["oneMin", '1m'],
+  ["fiveMin", '5m'],
+  ["thirtyMin", '30m'],
+  ["hour", '1H'],
+  ["day", '1D'],
+];
 
-var pair = "USDT-BTC";
-var interval = "thirtyMin";
+var interval = INTERVALS[1];
 
-var url = `${BITTREX_PROXY}?marketName=${pair}&tickInterval=${interval}&_=1499127220008`
+var url = `${BITTREX_PROXY}?marketName=${pair}&tickInterval=${interval[0]}&_=1499127220008`
 
 console.info("Proxying to BITTREX...");
+
+var fourier = new Fourier();
 
 fetch(url).then(function(response) {
     var contentType = response.headers.get("content-type");
@@ -38,13 +47,21 @@ fetch(url).then(function(response) {
 function render(r)
 {
   // which bar to render from left edge of the chart window onwards
-  var offset = (MAX_DISPLAY_BARS / 2)>>0;
+  var offset = (MAX_DISPLAY_BARS/2)>>0;
 
   // how many bars to render on screen
   var numbars = MAX_DISPLAY_BARS;
   var bars = r.result.reverse();
 
+  var res = fourier.extrapolate(bars);
+
+  console.log(res.past.data);
+  console.log(res.past.offset);
+  console.log(res.future.data);
+  console.log(res.future.offset);
+
   var end = Math.max(offset - (numbars-1), 0);
+
 
   var output = [];
 
@@ -57,11 +74,75 @@ function render(r)
                   Math.max.apply(Math,output.map(function(o){return o.H;}))];
 
   draw_chart(output, range);
+  draw_fourier(offset, res, range);
+
+  ctx.font = "14px Courier";
+  ctx.fillStyle = 'yellow';
+  ctx.fillText(`${pair}`,10,20);
+  ctx.fillStyle = 'white';
+  ctx.fillText(`${exchange}, ${interval[1]}`,10,40);
+
 }
+
+
+function draw_fourier(screen_column_latest, f, range)
+{
+  var past_coords = [];
+  for (var t=0; t<f.past.data.length; t++)
+  {
+    var column_x = (((screen_column_latest - t) + f.past.offset) * BAR_FOOTPRINT)>>0;
+    if (column_x < 0) break;// {console.log(t, column, "BREAK"); break;}
+    var price_y = ((1 - logical(f.past.data[t], range)) * CHART_HEIGHT)>>0;
+    // console.log(column, price_y);
+    past_coords.push([column_x, price_y]);
+  }
+
+  var future_coords = [];
+
+  for (var t=0; t<f.future.data.length; t++)
+  {
+    var column_x = (((screen_column_latest + t) + f.past.offset) * BAR_FOOTPRINT)>>0;
+    if (column_x > 1200) break;
+    var price_y = ((1 - logical(f.future.data[t], range)) * CHART_HEIGHT)>>0;
+    future_coords.push([column_x, price_y]);
+  }
+
+  ctx.lineWidth = 2;
+
+  for (var t=0; t<past_coords.length-1; t++)
+  {
+    var p1 = past_coords[t];
+    var p2 = past_coords[t+1];
+
+    ctx.strokeStyle = "white";
+    ctx.beginPath();
+    ctx.moveTo(p1[0], p1[1]);
+    ctx.lineTo(p2[0], p2[1]);
+    ctx.stroke();
+
+  }
+
+
+  for (var t=0; t<future_coords.length-1; t++)
+  {
+    var p1 = future_coords[t];
+    var p2 = future_coords[t+1];
+
+    ctx.strokeStyle = "blue";
+    ctx.beginPath();
+    ctx.moveTo(p1[0], p1[1]);
+    ctx.lineTo(p2[0], p2[1]);
+    ctx.stroke();
+
+  }
+
+}
+
 
 function draw_chart(output, range)
 {
   // logical 0..1 price coordinate = (VAL - LOWER) / RANGE
+
 
   ctx.fillStyle = "000000";
   ctx.lineWidth = 1;
